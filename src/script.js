967507699d571531c5346d8ccbad34ae67731bd3
AlexViy <alexviy@gmail.com>
2018-02-11 22:06:56 +0200

    Fefactored view

--- src/script.js
+++ src/script.js
@@ -1,12 +1,16 @@
-window.onload =  function() {
-  var myRequest = new XMLHttpRequest();
-  myRequest.open('GET', 'https://ws.fanteam.com/match_collections?sport=football&tab=admin_created&statuses[]=waiting&page=0&per_page=30&bearer[white_label]=fanteam');
-  myRequest.onload = function() {
-    var fanTeamObj =  JSON.parse(this.responseText);
-  function ViewModel() {
-    this.tournaments = ko.observableArray(fanTeamObj.tournaments);
-  }
-  ko.applyBindings(new ViewModel());
+function ViewModel() {
+  var self = this;
+  self.data = function() {
+    var myRequest = new XMLHttpRequest();
+    myRequest.open('GET', 'https://ws.fanteam.com/match_collections?sport=football&tab=admin_created&statuses[]=waiting&page=0&per_page=30&bearer[white_label]=fanteam');
+    myRequest.onload = function() {
+      var fanTeamObj =  JSON.parse(this.responseText);
+      self.tournaments(fanTeamObj.tournaments)
+    };
+    myRequest.send();
   };
-  myRequest.send();
-};
+
+  self.tournaments = ko.observableArray(self.data());
+}
+
+ko.applyBindings(new ViewModel());
